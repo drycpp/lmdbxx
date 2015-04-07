27a6170f087548d2bba59cad9b42b3ad9c4d514f
Arto Bendiken <arto@bendiken.net>
2015-04-07 13:52:33 +0200

    Created the project.

---
+++ lmdb++.h
@@ -0,0 +1,16 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#ifndef LMDBXX_H
+#define LMDBXX_H
+
+/**
+ * <lmdb++.h>
+ */
+
+#ifndef __cplusplus
+#error "<lmdb++.h> requires a C++ compiler"
+#endif
+
+namespace lmdb {}
+
+#endif /* LMDBXX_H */
